71d8654da5b40bb4b2c58f0f2a6ebb3d6bef9b49
ZhdanovskikhAV <95285003+ZhdanovskikhAV@users.noreply.github.com>
2024-08-22 19:16:41 +0300

    hw02_fix_app v7

--- hw02_fix_app/printer/pkg.go
+++ hw02_fix_app/printer/pkg.go
@@ -7,24 +7,14 @@ import (
 )
 
 func PrintStaff(staff []types.Employee) {
-
 	for i := 0; i < len(staff); i++ {
-
 		str := fmt.Sprintf(
-
 			"User ID: %d; Age: %d; Name: %s; Department ID: %d; ",
-
 			staff[i].UserID,
-
 			staff[i].Age,
-
 			staff[i].Name,
-
 			staff[i].DepartmentID,
 		)
-
 		fmt.Println(str)
-
 	}
-
 }
