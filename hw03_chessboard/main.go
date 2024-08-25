2b25a1eee3255a825407678a7c55d1ebaaf2b5e9
ZhdanovskikhAV <95285003+ZhdanovskikhAV@users.noreply.github.com>
2024-08-25 12:58:19 +0300

    hw03_chessboard_v1

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,37 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	var size int
+	fmt.Println("Введите число клеток в ряду: ")
+	fmt.Scanf("%d\n", &size)
+	for row := 0; row < size; row++ {
+		var (
+			str = ""
+			a   = ""
+			b   = ""
+		)
+
+		if row%2 == 0 {
+			a = "#"
+			b = " "
+		} else {
+			a = " "
+			b = "#"
+		}
+
+		for column := 0; column < size; column++ {
+			if column%2 == 0 {
+				str = str + a
+
+			} else {
+				str = str + b
+
+			}
+
+		}
+		fmt.Printf("%s\n", str)
+
+	}
 }
