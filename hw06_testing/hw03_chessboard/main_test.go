03ab5f2cfc9eb709726034717bf2c07fbb273d00
ZhdanovskikhAV <95285003+ZhdanovskikhAV@users.noreply.github.com>
2024-09-16 22:50:46 +0300

    тест шахм доска

--- hw06_testing/hw03_chessboard/main_test.go
+++ hw06_testing/hw03_chessboard/main_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 )
 
-// Функция для генерации доски в виде строки
 func generateBoard(size int) string {
 	var sb strings.Builder
 	for row := 0; row < size; row++ {
@@ -15,24 +14,17 @@ func generateBoard(size int) string {
 	return sb.String()
 }
 
-// Тестирование функции генерации доски
 func TestGenerateBoard(t *testing.T) {
-	// Ожидаемая доска для размера 3
 	expectedBoard := "# #\n # \n# #\n"
-
-	// Генерируем доску
 	board := generateBoard(3)
 
-	// Сравниваем результат
 	if board != expectedBoard {
 		t.Errorf("Expected:\n%sGot:\n%s", expectedBoard, board)
 	}
 
-	// Ожидаемая доска для размера 4
 	expectedBoard = "# # \n # #\n# # \n # #\n"
 	board = generateBoard(4)
 
-	// Сравниваем результат
 	if board != expectedBoard {
 		t.Errorf("Expected:\n%sGot:\n%s", expectedBoard, board)
 	}
