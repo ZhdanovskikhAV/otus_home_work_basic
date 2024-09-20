b6bb1ce5f85488520b14b9c04aa6d83dba99a1b6
ZhdanovskikhAV <95285003+ZhdanovskikhAV@users.noreply.github.com>
2024-09-21 00:00:38 +0300

    подсчет слов и юнит тест

--- hw07_word_counter/main.go
+++ hw07_word_counter/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+func countWords(text string) map[string]int {
+	text = strings.ToLower(text)
+
+	re := regexp.MustCompile(`[^\p{L}\s]+`)
+	text = re.ReplaceAllString(text, " ")
+
+	words := strings.Fields(text)
+
+	wordCount := make(map[string]int)
+
+	for _, word := range words {
+		wordCount[word]++
+	}
+
+	return wordCount
+}
+
 func main() {
-	// Place your code here.
+	text := "Привет, мир! Привет всем. Мир! Это тест, это тест!"
+	wordCount := countWords(text)
+	fmt.Println(wordCount)
 }
