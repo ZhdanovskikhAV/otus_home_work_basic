7a17efa382eb4fe7f3b88a1729a905a8311d7b86
ZhdanovskikhAV <95285003+ZhdanovskikhAV@users.noreply.github.com>
2024-09-21 00:17:19 +0300

    исправление ошибки линтеров

--- hw07_word_counter/main_test.go
+++ hw07_word_counter/main_test.go
@@ -11,14 +11,34 @@ func TestCountWords(t *testing.T) {
 		input    string
 		expected map[string]int
 	}{
-		{"Hello, world! Hello everyone. This is a test.", map[string]int{"hello": 2, "world": 1, "everyone": 1, "this": 1, "is": 1, "a": 1, "test": 1}},
-		{"Go is great! Go is fast.", map[string]int{"go": 2, "is": 2, "great": 1, "fast": 1}},
-		{"", map[string]int{}},
-		{"Go, Python, and Java are programming languages.", map[string]int{"go": 1, "python": 1, "and": 1, "java": 1, "are": 1, "programming": 1, "languages": 1}},
+		{
+			input:    "Hello, world! Hello everyone. This is a test.",
+			expected: map[string]int{"hello": 2, "world": 1, "everyone": 1, "this": 1, "is": 1, "a": 1, "test": 1},
+		},
+		{
+			input:    "Go is great! Go is fast.",
+			expected: map[string]int{"go": 2, "is": 2, "great": 1, "fast": 1},
+		},
+		{
+			input:    "",
+			expected: map[string]int{},
+		},
+		{
+			input:    "Go, Python, and Java are programming languages.",
+			expected: map[string]int{"go": 1, "python": 1, "and": 1, "java": 1, "are": 1, "programming": 1, "languages": 1},
+		},
 	}
 
 	for _, test := range tests {
 		output := countWords(test.input)
-		assert.Equal(t, test.expected, output, "For input '%s', expected %v, but got %v", test.input, test.expected, output)
+		assert.Equal(
+			t,
+			test.expected,
+			output,
+			"For input '%s', expected %v, but got %v",
+			test.input,
+			test.expected,
+			output,
+		)
 	}
 }
