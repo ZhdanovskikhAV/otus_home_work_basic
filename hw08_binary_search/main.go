6788c8353bd4c127d86b22b1a4e402f116f3cb69
ZhdanovskikhAV <95285003+ZhdanovskikhAV@users.noreply.github.com>
2024-09-28 00:10:46 +0300

    двоичный поиск и тест

--- hw08_binary_search/main.go
+++ hw08_binary_search/main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"fmt"
+)
+
+func binarySearch(arr []int, target int) int {
+	left, right := 0, len(arr)-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+		midValue := arr[mid]
+
+		switch {
+		case midValue == target:
+			return mid
+		case midValue < target:
+			left = mid + 1
+		case midValue > target:
+			right = mid - 1
+		}
+	}
+
+	return -1
+}
+
 func main() {
-	// Place your code here.
+	data := []int{1, 3, 5, 7, 9, 11, 13, 15, 17, 19}
+	target := 11
+	result := binarySearch(data, target)
+
+	if result != -1 {
+		fmt.Printf("Элемент %d найден по индексу %d.\n", target, result)
+	} else {
+		fmt.Printf("Элемент %d не найден.\n", target)
+	}
 }
