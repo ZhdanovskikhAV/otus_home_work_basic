75e40b402a532cb467ee034dcbf239725b77a058
ZhdanovskikhAV <95285003+ZhdanovskikhAV@users.noreply.github.com>
2024-12-23 01:23:34 +0300

    docker

--- hw16_docker/main.go
+++ hw16_docker/main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+    "fmt"
+    "log"
+    "os"
+    
+    "github.com/ZhdanovskikhAV/otus_home_work_basic/hw16_docker/pkg/server"
+    "github.com/joho/godotenv"
+)
+
 func main() {
-	// Place your code here.
+    // Попытка загрузить переменные из файла .env
+    envFile := ".env"
+    if _, err := os.Stat(envFile); err == nil {
+        err := godotenv.Load(envFile)
+        if err != nil {
+            log.Fatal("Error loading .env file")
+        }
+    }
+
+    // Получаем значения переменных окружения
+    dsn := os.Getenv("DB_DSN")
+    ip := os.Getenv("APP_HOST")
+    port := os.Getenv("APP_HTTP_PORT")
+    fmt.Printf("DB_DSN: %s\n", dsn)
+    fmt.Printf("APP_HOST: %s\n", ip)
+    fmt.Printf("APP_HTTP_PORT: %s\n", port)
+    if len(dsn) == 0 || len(ip) == 0 || len(port) == 0 {
+        fmt.Println("DB_DSN or APP_HOST or APP_HTTP_PORT not set")
+        return
+    }
+
+    server.Server(&ip, &port, &dsn)
 }
